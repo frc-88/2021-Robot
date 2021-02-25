e52d3f61e688ef939f94b59d373ba30e2d0bf407
Nick Avtges <navtges@gmail.com>
2021-02-25 16:00:22 -0500

    Barrel run tweaks...- 0.25s?

--- src/main/java/frc/robot/util/GameChangerTrajectories.java
+++ src/main/java/frc/robot/util/GameChangerTrajectories.java
@@ -67,10 +67,8 @@ public class GameChangerTrajectories {
         waypoints.add(new Translation2d(Units.feetToMeters(12.5), Units.feetToMeters(7.5)));
         // around the second barrel
         waypoints.add(new Translation2d(Units.feetToMeters(21.0), Units.feetToMeters(7.5)));
-        waypoints.add(new Translation2d(Units.feetToMeters(25.0), Units.feetToMeters(10.5)));
-        waypoints.add(new Translation2d(Units.feetToMeters(18.5), Units.feetToMeters(12.8)));
-        // waypoints.add(new Translation2d(Units.feetToMeters(18.5),
-        // Units.feetToMeters(10.0)));
+        waypoints.add(new Translation2d(Units.feetToMeters(25.0), Units.feetToMeters(12.5)));
+        waypoints.add(new Translation2d(Units.feetToMeters(19.0), Units.feetToMeters(12.8)));
         // around the third barrel
         waypoints.add(new Translation2d(Units.feetToMeters(25.0), Units.feetToMeters(2.0)));
         waypoints.add(new Translation2d(Units.feetToMeters(29.0), Units.feetToMeters(5.0)));
